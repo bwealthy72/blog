7504af783568ab34726ec3915dd6f2e7e5d8c5de
bwealthy <bwealthy72@gmail.com>
2022-01-26 09:46:11 +0900

    Initial Commit

---
+++ pages/_category/_post.vue
@@ -0,0 +1,28 @@
+<template>
+  <main class="post-wrapper">
+    <div class="header">
+      <button class="category"></button>
+      <h1 class="title"></h1>
+      <p class="date">
+        <span class="created"></span>
+        <span class="updated"></span>
+      </p>
+    </div>
+    <article class="post">
+      <div class="toc"></div>
+      <div class="content">
+        <div class="tags"></div>
+        <div class="cover-img">
+          <img src="" alt="" />
+        </div>
+        <div class="realcontent"></div>
+      </div>
+    </article>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ pages/_category/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <main class="category-wrapper">
+    <div class="header"></div>
+    <article>
+      <h1 class="category"></h1>
+      <ul class="post-list">
+        <li class="post">
+          <p class="title"></p>
+          <!-- 앞에 달력 아이콘을 붙이면 예쁠 듯 -->
+          <p class="date"></p>
+          <ul class="tags">
+            <li class="tag"></li>
+          </ul>
+        </li>
+      </ul>
+    </article>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
