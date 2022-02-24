ef0f4ee529702a8caaa108adf4356fcab31fbcf7
bwealthy <bwealthy72@gmail.com>
2022-02-24 14:45:17 +0900

    Header와 Footer에 광고 추가

--- components/layout/Footer.vue
+++ components/layout/Footer.vue
@@ -1,6 +1,9 @@
 <template>
   <footer class="site-footer">
-    <div class="site-info">
+    <div class="site-footer__ad">
+      <Ad adslot="3445169841"></Ad>
+    </div>
+    <div class="site-footer__info">
       <span class="site-info__copyright">
         Copyright © 2022 by BWealthy all rights reserved.
       </span>
--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,9 +1,13 @@
 <template>
   <header class="site-header">
-    <a href="/" class="site-logo">
+    <a href="/" class="site-header__logo">
       <img src="~/assets/images/logo.png" alt="" />
     </a>
     <Search></Search>
+
+    <div class="site-header__ad">
+      <Ad adslot="3845475151"></Ad>
+    </div>
     <Navigation></Navigation>
   </header>
 </template>
