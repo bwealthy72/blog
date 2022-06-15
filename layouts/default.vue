1645f8965acc0e8f7d17ed77a774fb231bf78a08
bwealthy <bwealthy72@gmail.com>
2022-06-15 22:29:31 +0900

    mobile 체크 제거

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,7 @@
 <template>
   <div class="container">
-    <DesktopDock v-if="!$store.state.isMobile"></DesktopDock>
-    <DesktopHeader v-if="!$store.state.isMobile"></DesktopHeader>
+    <DesktopDock></DesktopDock>
+    <DesktopHeader></DesktopHeader>
     <Nuxt />
   </div>
 </template>
-
-<script>
-export default {
-  middleware(ctx) {
-    if (process.server) {
-      // const userAgent = ctx.req.headers["user-agent"];
-      // const isMobile =
-      //   userAgent.match(
-      //     /iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i
-      //   ) != null || userAgent.match(/LG|SAMSUNG|Samsung/) != null;
-      // ctx.store.commit("setIsMobile", isMobile);
-    }
-  },
-};
-</script>
