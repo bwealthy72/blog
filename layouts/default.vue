0b37cb4598e00bbc8d9b172853ab9ca5195aca2d
bwealthy <bwealthy72@gmail.com>
2022-06-15 21:41:53 +0900

    모바일에 맞춰서 v-if가 적용되는지 확인

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="container">
-    <DesktopDock></DesktopDock>
-    <DesktopHeader></DesktopHeader>
+    <DesktopDock v-if="!$store.state.isMobile"></DesktopDock>
+    <DesktopHeader v-if="!$store.state.isMobile"></DesktopHeader>
     <Nuxt />
   </div>
 </template>
+
+<script>
+export default {
+  middleware(ctx) {
+    if (process.server) {
+      // const userAgent = ctx.req.headers["user-agent"];
+      // const isMobile =
+      //   userAgent.match(
+      //     /iPhone|iPod|Android|Windows CE|BlackBerry|Symbian|Windows Phone|webOS|Opera Mini|Opera Mobi|POLARIS|IEMobile|lgtelecom|nokia|SonyEricsson/i
+      //   ) != null || userAgent.match(/LG|SAMSUNG|Samsung/) != null;
+      // ctx.store.commit("setIsMobile", isMobile);
+    }
+  },
+};
+</script>
